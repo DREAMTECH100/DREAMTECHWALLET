c8afe9accf6ac74f5ebea8fdf8018743113d5162
DREAMTECH100 <stacyjordan236@gmail.com>
2025-11-14 11:51:00 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/balance/' // ← important for GitHub Pages deployment
+  base: '/DREAMTECHWALLET/' 
 });
